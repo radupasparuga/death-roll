0600f0b84adeee39974a184c3183127b56b23228
radupasparuga <radupasparuga25@gmail.com>
2020-12-15 21:59:39 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+from random import randrange
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+    	  return
+
+    if message.content.startswith('!roll'):
+        content = message.content.split()
+        response = randrange(int(content[1]))+1
+        if response == 1:
+            response = '<:YouDied:788477968976052224>'
+        await message.channel.send(response)
+
+client.run(TOKEN)
